23b95b5f0818bea653ad7ed0d3ecce50d14df937
Gocha Avsajanishvili <gochaavsajanishvili@gmail.com>
2024-11-23 18:31:17 +0400

    Update metadata.ts

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -4,8 +4,8 @@ import { Metadata } from 'next'
 
 export function constructMetadata({
   title = 'Gocha Avsajanishvili', // TODO: Add a custom title
-  description = "I am a software engineer with a passion for building scalable and efficient web applications.", // TODO: Add a custom description
-  image = '/thumbnail.png', // TODO: Add a custom image
+  description = "I'm dedicated to crafting responsive websites with catchy animations and strong back-end implementation which drive client's digital success.", // TODO: Add a custom description
+  image = '/1613159192979.jpg', // TODO: Add a custom image
   icons = '/avatar.svg', // TODO: Add a custom icon
   noIndex = false
 }: {
